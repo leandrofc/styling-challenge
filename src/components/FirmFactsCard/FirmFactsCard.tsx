66cba5638970d6702f46199a965c86e638743661
Leandro Fonseca <leandromkt@outlook.com>
2025-02-13 00:26:24 -0300

    feat: add disabled and children position prop in the FirmFactsCard component

--- src/components/FirmFactsCard/FirmFactsCard.tsx
+++ src/components/FirmFactsCard/FirmFactsCard.tsx
@@ -3,7 +3,7 @@ import styles from './FirmFactsCard.module.scss';
 import { FirmFactsCardProps } from './FirmFactsCard.types';
 import React from 'react';
 
-const FirmFactsCard = ({ children, variant = 'default' }: FirmFactsCardProps) => {
+const FirmFactsCard = ({ children, variant = 'default', disabled, childrenPosition = 'center' }: FirmFactsCardProps) => {
     const [isHovered, setIsHovered] = useState(false);
 
     const handleMouseEnter = () => setIsHovered(true);
@@ -14,14 +14,19 @@ const FirmFactsCard = ({ children, variant = 'default' }: FirmFactsCardProps) =>
     return (
         <div className={`
                 ${styles['firm-facts-card']}
-                ${styles[`firm-facts-card--${variant}`]}
+                ${!disabled && isHovered && styles[`firm-facts-card--${variant}--isHovered`]}
+                ${disabled && styles['firm-facts-card--disableHover']}
+                ${styles[`firm-facts-card--${childrenPosition}`]}
             `}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
             {React.Children.map(children, (child) => {
                 return React.cloneElement(
-                    child as React.ReactElement<any>, { isHovered, hasborder: isDefaultVariant }
+                    child as React.ReactElement<any>, {
+                        isHovered: isHovered && !disabled,
+                        hasborder: isDefaultVariant
+                    }
                 );
             })}
         </div>
--- src/components/FirmFactsCard/FirmFactsCard.types.ts
+++ src/components/FirmFactsCard/FirmFactsCard.types.ts
@@ -3,4 +3,6 @@ import { ReactNode } from "react";
 export type FirmFactsCardProps = {
     variant?: 'default' | 'border';
     children: ReactNode;
+    disabled?: boolean;
+    childrenPosition?: 'top' | 'center';
 };
