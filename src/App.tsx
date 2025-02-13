d30c061518ec162b15c93f037aedad76548a2543
Leandro Fonseca <leandromkt@outlook.com>
2025-02-13 18:10:02 -0300

    feat: render FirmFacts on the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,83 @@
+import FirmFacts from './components/FirmFacts';
+import { FirmFactsProps } from './components/FirmFacts/FirmFacts.types';
+
 function App() {
-  return (
-    <>
-      <p style={{ color: "black" }}>Hello World</p>
-    </>
-  )
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  const content: FirmFactsProps = [
+    {
+      buttonProps: {
+        label: "This is a two line button that terminates with anything else",
+        onClick: handleClick,
+        variant: "icon"
+      },
+      isTwoRows: true,
+      index: 1,
+    },
+    {
+      buttonProps: {
+        label: "This is a two line button that terminates with anything else",
+        onClick: handleClick,
+        variant: "icon"
+      },
+      index: 2,
+    },
+    {
+      buttonProps: {
+        label: "This is a one line button",
+        onClick: handleClick,
+      },
+      index: 3,
+    },
+    {
+      buttonProps: {
+        label: "This is a two line button that terminates with anything else",
+        onClick: handleClick,
+        variant: "icon"
+      },
+      index: 4,
+    },
+    {
+      buttonProps: {
+        label: "This is a two line button that terminates with anything else",
+        onClick: handleClick,
+        variant: "icon"
+      },
+      cardProps: {
+        childrenPosition: "top",
+      },
+      index: 5,
+    },
+    {
+      buttonProps: {
+        label: "This is a two line button that terminates with anything else",
+        onClick: handleClick,
+        variant: "icon",
+        disabled: true,
+      },
+      cardProps: {
+        childrenPosition: "top",
+        disabled: true,
+      },
+      index: 6,
+    },
+    {
+      buttonProps: {
+        label: "This is a two line button that terminates with anything else",
+        onClick: handleClick,
+        variant: "icon",
+      },
+      cardProps: {
+        childrenPosition: "top",
+        variant: "border",
+      },
+      index: 7,
+    }
+  ]
+
+  return <FirmFacts data={content} />
 }
 
 export default App
